f3136e54248c28427a1c456281df9e3a42135420
Daniel Elliot <danielelliotr@gmail.com>
2021-02-10 14:23:30 -0800

    DELETE Route

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,11 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 22
-  },
+let users = []
 
-]
 
 router.get('/', (req, res) => {
   res.send(users)
@@ -23,9 +14,25 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const user = req.body
 
-  users.push(user)
+  users.push({ ...user, id: uuidv4() })
 
   res.send(`User with the name ${user.firstName} added to the Database!`)
-})
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id)
+
+  res.send(foundUser)
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with the id ${id} deleted from the database.`)
+});
 
 export default router;
